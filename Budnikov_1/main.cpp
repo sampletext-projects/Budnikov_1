f9f434a213e9ee010e7b9fc8295d531b3026323a
sampletext32 <sampletext32@bk.ru>
2020-04-11 19:54:00 +0300

    Add project

---
+++ Budnikov_1/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> // для cout
+#include <iomanip> //Для setw
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	int a = 0;
+	int a2 = 1;
+	int a3 = 1;
+
+	cout << "Таблица кубов чисел a от 1 до 10\n";
+	
+	while (a < 10)
+	{
+		a++;
+		a2 = a * a;
+		a3 = a * a * a;
+		cout << setw(4) << a2 << " " << setw(4) << a3 << "\n";
+	}
+
+	return 0;
+}
